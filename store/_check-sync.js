596db46d8961f68a7bc355ce6bc7881a848d4572
Justin Diaz <justin.sinclair1208@gmail.com>
2026-07-12 22:45:56 -0400

    Remove accidentally committed temp file

--- store/_check-sync.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const { neon } = require('@neondatabase/serverless');
-const sql = neon(process.env.DATABASE_URL);
-sql`SELECT COUNT(*) c FROM cards WHERE last_price_sync > NOW() - INTERVAL '5 minutes'`
-  .then((r) => console.log('Cards synced in last 5 min:', r[0].c))
-  .catch((e) => console.error(e.message));
